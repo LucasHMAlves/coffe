d6a931d2b7d355f7260032760e1e0917a3ea397a
Lucas Henrique Muzulon Alves <lucasmuzuzu@gmail.com>
2023-05-14 08:20:47 -0300

    primeiro commit

---
+++ coffe.py
@@ -0,0 +1,12 @@
+while True:
+    coffee = "coffe situation."
+    if coffee == "empty":
+        print("fill")
+        break
+    elif coffee == "full":
+        print('drink')
+        break
+    else:
+        print("it's ok?")
+    pass
+print('so proceeed......☕')
